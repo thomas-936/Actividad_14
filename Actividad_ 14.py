444ff4ec8e05c7a6857086967b42be45f4c07f12
tomas <tomas27@gmail.com>
2025-08-08 21:08:19 -0600

    1.

--- Actividad_ 14.py
+++ Actividad_ 14.py
@@ -1,1 +1,17 @@
 print("Actividad 14")
+
+class Competidor:
+    def __init__(self, num_dorsal, nombre, edad, categoria):
+        self.num_dorsal = num_dorsal
+        self.nombre = nombre
+        self.edad = edad
+        self.categoria = categoria
+
+class SistemaCarrera:
+    def menu(self):
+        opcion = 0
+        while opcion!= 4:
+            print("1. Agregar participante")
+            print("2. Mostar participantes ordenados por nombre")
+            print("3. Mostar participantes ordenados por edad")
+            print("4. Salir")
